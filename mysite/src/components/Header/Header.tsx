058185c3efc902a31eb60a367ad1a82511ca2b67
vitoremerique <vitort.emerique@hotmail.com>
2024-11-19 15:11:34 -0300

    improve css

--- mysite/src/components/Header/Header.tsx
+++ mysite/src/components/Header/Header.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header: React.FC = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">{`</>`}</div>
-      <nav className="nav">
-        <a href="#introduction">Home</a>
-        <a href="#about">About Me</a>
-        <a href="#scientific">Scientific Initiation</a>
-        <a href="#skills">Skills</a>
-        <a href="#contact">Contact</a>
+      {/* Ícone do menu mobile */}
+      <div className="menu-icon" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? '✖' : '☰'}
+      </div>
+      <nav className={`nav ${isMobileMenuOpen ? 'mobile-active' : ''}`}>
+        <a href="#introduction" onClick={() => setIsMobileMenuOpen(false)}>Home</a>
+        <a href="#about" onClick={() => setIsMobileMenuOpen(false)}>About Me</a>
+        <a href="#scientific" onClick={() => setIsMobileMenuOpen(false)}>Scientific Initiation</a>
+        <a href="#skills" onClick={() => setIsMobileMenuOpen(false)}>Skills</a>
+        <a href="#contact" onClick={() => setIsMobileMenuOpen(false)}>Contact</a>
       </nav>
     </header>
   );
